9c57e0d42ac25cff960e376cf5a3d7b7425e5a4e
shotgun45 <jipeles@yahoo.com>
2025-10-20 18:30:35 -0400

    Chapter 5 Lesson 7

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -20,18 +20,54 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Ack
 }
 
 // handlerMove creates a handler function for army move messages
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckType {
+func handlerMove(gs *gamelogic.GameState, pubCh *amqp091.Channel, username string) func(gamelogic.ArmyMove) pubsub.AckType {
 	return func(move gamelogic.ArmyMove) pubsub.AckType {
 		defer fmt.Print("> ")
 		outcome := gs.HandleMove(move)
 
 		// Determine acknowledgment type based on move outcome
 		switch outcome {
-		case gamelogic.MoveOutComeSafe, gamelogic.MoveOutcomeMakeWar:
+		case gamelogic.MoveOutComeSafe:
 			return pubsub.Ack
+		case gamelogic.MoveOutcomeMakeWar:
+			// Create and publish war recognition message
+			warMessage := gamelogic.RecognitionOfWar{
+				Attacker: move.Player,
+				Defender: gs.GetPlayerSnap(),
+			}
+
+			// Publish to war exchange with routing key war.username
+			warRoutingKey := fmt.Sprintf("%s.%s", routing.WarRecognitionsPrefix, username)
+			err := pubsub.PublishJSON(pubCh, routing.ExchangePerilTopic, warRoutingKey, warMessage)
+			if err != nil {
+				log.Printf("Failed to publish war message: %v", err)
+			}
+
+			return pubsub.NackRequeue
 		case gamelogic.MoveOutcomeSamePlayer:
 			return pubsub.NackDiscard
 		default:
+			return pubsub.NackDiscard
+		}
+	}
+}
+
+// handlerWar creates a handler function for war recognition messages
+func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.AckType {
+	return func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
+		defer fmt.Print("> ")
+		outcome, _, _ := gs.HandleWar(rw)
+
+		// Determine acknowledgment type based on war outcome
+		switch outcome {
+		case gamelogic.WarOutcomeNotInvolved:
+			return pubsub.NackRequeue
+		case gamelogic.WarOutcomeNoUnits:
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon, gamelogic.WarOutcomeYouWon, gamelogic.WarOutcomeDraw:
+			return pubsub.Ack
+		default:
+			fmt.Printf("Error: Unknown war outcome: %v\n", outcome)
 			return pubsub.NackDiscard
 		}
 	}
@@ -92,27 +128,42 @@ func main() {
 		log.Fatalf("Failed to subscribe to pause messages: %v", err)
 	}
 
-	// Subscribe to army moves from other players
-	moveQueueName := fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, username)
-	moveRoutingKey := fmt.Sprintf("%s.*", routing.ArmyMovesPrefix)
-	err = pubsub.SubscribeJSON(
-		conn,                       // connection
-		routing.ExchangePerilTopic, // exchange (topic exchange)
-		moveQueueName,              // queue name (army_moves.username)
-		moveRoutingKey,             // routing key (army_moves.*)
-		pubsub.Transient,           // queue type
-		handlerMove(gameState),     // handler function
-	)
-	if err != nil {
-		log.Fatalf("Failed to subscribe to army move messages: %v", err)
-	}
-
 	// Create a publishing channel for moves
 	pubCh, err := conn.Channel()
 	if err != nil {
 		log.Fatalf("Failed to create publishing channel: %v", err)
 	}
 	defer pubCh.Close()
+
+	// Subscribe to army moves from other players
+	moveQueueName := fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, username)
+	moveRoutingKey := fmt.Sprintf("%s.*", routing.ArmyMovesPrefix)
+	err = pubsub.SubscribeJSON(
+		conn,                                    // connection
+		routing.ExchangePerilTopic,              // exchange (topic exchange)
+		moveQueueName,                           // queue name (army_moves.username)
+		moveRoutingKey,                          // routing key (army_moves.*)
+		pubsub.Transient,                        // queue type
+		handlerMove(gameState, pubCh, username), // handler function
+	)
+	if err != nil {
+		log.Fatalf("Failed to subscribe to army move messages: %v", err)
+	}
+
+	// Subscribe to war messages from all players
+	warQueueName := routing.WarRecognitionsPrefix // Use shared "war" queue
+	warRoutingKey := fmt.Sprintf("%s.*", routing.WarRecognitionsPrefix)
+	err = pubsub.SubscribeJSON(
+		conn,                       // connection
+		routing.ExchangePerilTopic, // exchange (topic exchange)
+		warQueueName,               // queue name (war) - shared by all clients
+		warRoutingKey,              // routing key (war.*)
+		pubsub.Durable,             // queue type - should be durable for shared queue
+		handlerWar(gameState),      // handler function
+	)
+	if err != nil {
+		log.Fatalf("Failed to subscribe to war messages: %v", err)
+	}
 
 	// Start REPL loop
 	for {
